cc83b915f91a31dfd2d1e48ae59198d18603490b
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-12-01 16:02:45 +0530

    Services section added

---
+++ src/components/Services.js
@@ -0,0 +1,32 @@
+import "./Services.css"
+
+function Services() {
+    return (
+        <section id="section3">
+            <div class="grheading">My Quality Services</div>
+            <p class="headingtext">We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.</p>
+            <div class="servicetag">
+                <span>01</span>
+                <div>FrontEnd Development</div>
+                <div><p>I build clean, efficient, and interactive websites using HTML, CSS, and JavaScript. My focus is on delivering user-friendly interfaces with smooth functionality.</p></div>
+            </div>
+            <div class="servicetag">
+                <span>02</span>
+                <div>UI/UX Implementation</div>
+                <div><p>I convert wireframes or design mockups (from tools like Figma/PSD) into functional web pages, maintaining attention to detail and usability.</p></div>
+            </div>
+            <div class="servicetag">
+                <span>03</span>
+                <div>Responsive Web Design</div>
+                <div><p>I create fully responsive websites that adapt seamlessly to desktops, tablets, and mobile devices, ensuring an optimal user experience across all screen sizes.</p></div>
+            </div>
+            <div class="servicetag">
+                <span>04</span>
+                <div>Website Optimization</div>
+                <div><p>I optimize websites for performance, including faster load times, clean code structure, and SEO-friendly practices for better visibility.</p></div>
+            </div>
+        </section>
+    )
+}
+
+export default Services;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Services from './components/Services';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <Header />
     <Hero />
+    <Services />
   </React.StrictMode>
 );
 
