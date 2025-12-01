246d0b9eccba11a152e3356acff74c9932871f2a
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-12-01 14:45:15 +0530

    folder structure updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './Header.css';
-import logo from './assets/logo.png';
+import logo from '../assets/logo.png';
 
 function Header(){
     return (
         <header>
             <nav>
                 <div className="logo">
-                    <img src={logo} alt="logo" />
+                    <a href="#"><img src={logo} alt="logo" /></a>
                 </div>
                 <div className="menu">
                     <ul className="list">
