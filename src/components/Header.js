38f4b26ee6001fbdbd9ed83bbced30eeaa5e62e9
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-12-07 13:19:14 +0530

    Hamburger menu added for mobile responsive header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,38 @@
-import './Header.css';
-import logo from '../assets/logo.png';
-import menuConfig from '../config/menuConfig';
+import { useState } from "react";
+import "./Header.css";
+import logo from "../assets/logo.png";
+import menuConfig from "../config/menuConfig";
 
-function Header(){
-    return (
-        <header>
-            <nav>
-                <div className="logo">
-                    <a href="#"><img src={logo} alt="logo" /></a>
-                </div>
-                <div className="menu">
-                    <ul className="list">
-                        {menuConfig.map((item) => (
-                            <li key={item.id}><a href={item.link}>{item.label}</a></li>
-                        ))}
-                    </ul>
-                </div>
-                <a href="#contact" className="hire-me">Hire Me</a>
-                <span id="more">&#9776;</span>
-                </nav>
-        </header>
-    )
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <nav>
+        <div className="logo">
+          <a href="#">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+
+        <div className={`menu ${menuOpen ? "active" : ""}`}>
+          <ul className="list">
+            {menuConfig.map((item) => (
+              <li key={item.id} onClick={() => setMenuOpen(false)}>
+                <a href={item.link}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <a href="#contact" className="hire-me">Hire Me</a>
+
+        <span id="more" onClick={() => setMenuOpen(!menuOpen)}>
+          &#9776;
+        </span>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
